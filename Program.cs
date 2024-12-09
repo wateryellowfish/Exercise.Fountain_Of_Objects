09fb519c0da8ba3d19daf5b25d538c92f3237da0
Clarence Arcana <arcanaclarence@gmail.com>
2024-12-09 15:48:14 +0800

    converted coordinates to tuple

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿
+
 ChooseMapSize();
 Console.Clear();
 
 Map.InitializeTiles();
-Map.Mark("X", Player.Row,Player.Column);
+Map.Mark("X", Player.Location);
 Intro.ColoredMessage();
 
 while (!Player.PlayerWins)
 {
     Player.Move();
-    if(Player.Row==0 && Player.Column==0)
+    if(Player.Location==(0,0))
     {
         if (!Fountain.IsActivated)
         {
@@ -21,7 +22,7 @@ while (!Player.PlayerWins)
             Player.PlayerWins = true;
         }
     }
-    else if(Player.Row==Fountain.Row &&  Player.Column==Fountain.Column)
+    else if(Player.Location==Fountain.Location)
     {
         if(!Fountain.IsActivated)
         {
@@ -46,19 +47,12 @@ void ChooseMapSize()
     string? input = Console.ReadLine();
     if (int.TryParse(input, out int num) && num >= 1 && num <= 3)
     {
-        Map.Row = num switch
+        Map.Size = num switch
         {
-            1 => 4,
-            2 => 6,
-            3 => 8,
-            _ => 0
-        };
-        Map.Column = num switch
-        {
-            1 => 4,
-            2 => 6,
-            3 => 8,
-            _ => 0
+            1 => (4,4),
+            2 => (6,6),
+            3 => (8,8),
+            _ => (0,0)
         };
     }
     else
@@ -71,33 +65,32 @@ void ChooseMapSize()
 
 static class Map
 {
-    public static int Row { get; set; }
-    public static int Column { get; set; }
-    private static string[,] Tiles { get; set; } = new string[Row, Column];
+    public static (int row, int column) Size { get; set; }
+    private static string[,] Tiles { get; set; } = new string[Size.row, Size.column];
 
 
     public static void InitializeTiles()
     {
-        Tiles=new string[Row, Column];
-        for(int i=0; i<Row;i++)
+        Tiles=new string[Size.row, Size.column];
+        for(int i=0; i<Size.row;i++)
         {
-            for (int j = 0; j < Column; j++)     
+            for (int j = 0; j < Size.column; j++)     
             {
                 Tiles[i, j] = " ";
             }
         }
     }
 
-    public static void Mark(string mark, int row, int col)
+    public static void Mark(string mark, (int row, int col) location)
     {
         Console.WriteLine();
         int tilenum = 0;
-        for (int i = 0; i < Row; i++)
+        for (int i = 0; i < Size.row; i++)
         {
             Console.Write("|");
-            for (int j = 0; j < Column; j++)
+            for (int j = 0; j < Size.column; j++)
             {
-                if (i == row && j == col)
+                if (i == location.row && j == location.col)
                 {
                     Tiles[i,j]=mark;
                     Console.Write($" {Tiles[i,j]} |");
@@ -106,45 +99,48 @@ static class Map
                 tilenum++;
             }
             Console.WriteLine();
-            for(int k=0; k<=Column*4; k++)
+            for(int k=0; k<=Size.column*4; k++)
             {
                 Console.Write("-");
             }
             Console.WriteLine();
         }
     }
+
+    public static void RemovePlayerMark()
+    {
+        Tiles[Player.Location.row, Player.Location.column] = " ";
+    }
 }
 
 
 interface ILocation
 {
-    public static int Row { get; }
-    public static int Column { get; }
+    public static (int row, int column) Location { get; }
 }
 
 class Player : ILocation
 {
     public static bool PlayerWins { get; set; } = false;
-    public static int Row { get; private set; } = 0;
-    public static int Column { get; private set; } = 0;
+    public static (int row, int column) Location { get; private set; } = (0, 0);
     public static void Move()
     {
+        int row = Location.row;
+        int column = Location.column;
         ConsoleKey key=Console.ReadKey().Key;
-        Map.Mark(" ", Row, Column);
+        Map.RemovePlayerMark();
         Console.Clear();
-        if (key == ConsoleKey.RightArrow && (Column + 1) < Map.Column) Column++;
-        else if (key == ConsoleKey.LeftArrow && (Column - 1) >= 0) Column--;
-        else if (key == ConsoleKey.UpArrow && (Row - 1) >= 0) Row--;
-        else if (key == ConsoleKey.DownArrow && (Row + 1) < Map.Row) Row++;
-        Map.Mark("X", Row, Column);
+        if (key == ConsoleKey.RightArrow && (column + 1) < Map.Size.column) Location=(row,++column);
+        else if (key == ConsoleKey.LeftArrow && (column - 1) >= 0) Location= (row,--column);
+        else if (key == ConsoleKey.UpArrow && (row - 1) >= 0) Location=(--row,column);
+        else if (key == ConsoleKey.DownArrow && (row + 1) < Map.Size.row) Location= (++row,column);
+        Map.Mark("X", Location);
     }
 }
  class Fountain : ILocation
 {
     public static bool IsActivated { get; private set; } = false;
-    public static int Row { get; } = 0;
-    public static int Column { get; } = 2;
-
+    public static (int row, int col) Location { get; } = (0, 2);
 
     public static void Reactivate()
     {
@@ -159,8 +155,6 @@ class Player : ILocation
         else Console.WriteLine("Fountain not activated.");
     }
 }
-
-
 
 
 public abstract class Narrative
